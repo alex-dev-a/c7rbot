e111530c4f1b48c49bea13540c8cbe43cd2f1559
alex-dev-a <ahcenezemmari18@gmail.com>
2026-09-12 09:52:53 +0100

    Update removepoints.js

--- commands/removepoints.js
+++ commands/removepoints.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { readDb, writeDb, getStaff } = require('../utils/db');
-const { sendLog } = require('../utils/logger');
+const { readDb, writeDb, getStaff, logPointEvent } = require('../utils/db');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,14 +8,14 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .addUserOption(opt => opt.setName('عضو').setDescription('العضو').setRequired(true))
     .addIntegerOption(opt => opt.setName('عدد').setDescription('عدد النقاط').setRequired(true)),
-  async execute(interaction, client) {
+  async execute(interaction) {
     const user = interaction.options.getUser('عضو');
     const amount = interaction.options.getInteger('عدد');
     const db = readDb();
     const staff = getStaff(db, user.id);
     staff.points = Math.max(0, staff.points - amount);
+    logPointEvent(db, { type: 'manual', userId: user.id, amount: -amount, by: interaction.user.id });
     writeDb(db);
     await interaction.reply(`✅ تم خصم ${amount} نقطة من <@${user.id}>. الرصيد الحالي: ${staff.points}.`);
-    sendLog(client, '➖ خصم نقاط', `<@${interaction.user.id}> خصم ${amount} نقطة من <@${user.id}>.`);
   }
 };
