4838b1e7b8e98323515814fde2fa930f0cce76db
alex-dev-a <ahcenezemmari18@gmail.com>
2026-09-12 09:51:35 +0100

    Update addpoints.js

--- commands/addpoints.js
+++ commands/addpoints.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { readDb, writeDb, getStaff } = require('../utils/db');
-const { sendLog } = require('../utils/logger');
+const { readDb, writeDb, getStaff, logPointEvent } = require('../utils/db');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,14 +8,14 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .addUserOption(opt => opt.setName('عضو').setDescription('العضو').setRequired(true))
     .addIntegerOption(opt => opt.setName('عدد').setDescription('عدد النقاط').setRequired(true)),
-  async execute(interaction, client) {
+  async execute(interaction) {
     const user = interaction.options.getUser('عضو');
     const amount = interaction.options.getInteger('عدد');
     const db = readDb();
     const staff = getStaff(db, user.id);
     staff.points += amount;
+    logPointEvent(db, { type: 'manual', userId: user.id, amount, by: interaction.user.id });
     writeDb(db);
     await interaction.reply(`✅ تم إضافة ${amount} نقطة لـ <@${user.id}>. الرصيد الحالي: ${staff.points}.`);
-    sendLog(client, '➕ إضافة نقاط', `<@${interaction.user.id}> أضاف ${amount} نقطة لـ <@${user.id}>.`);
   }
 };
