e2ed4f41e2e15f9dc0357225dc9f7fee043b2a07
alex-dev-a <ahcenezemmari18@gmail.com>
2026-09-12 09:54:23 +0100

    Update claim.js

--- commands/claim.js
+++ commands/claim.js
@@ -1,18 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { readDb, writeDb, getStaff } = require('../utils/db');
-const { sendLog } = require('../utils/logger');
+const { readDb, writeDb, getStaff, logPointEvent } = require('../utils/db');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('استلام-تذكرة')
     .setDescription('تسجيل استلامك لتذكرة (يُستخدم مع بوتات التذاكر الخارجية)'),
-  async execute(interaction, client) {
+  async execute(interaction) {
     const db = readDb();
     const staff = getStaff(db, interaction.user.id);
     staff.points += db.settings.ticketClaimPoints;
     staff.ticketsHandled += 1;
+    logPointEvent(db, { type: 'ticket', userId: interaction.user.id, amount: db.settings.ticketClaimPoints, channelId: interaction.channel.id });
     writeDb(db);
     await interaction.reply(`✅ تم تسجيل استلامك للتذكرة (+${db.settings.ticketClaimPoints} نقطة).`);
-    sendLog(client, '📥 استلام تذكرة (يدوي)', `<@${interaction.user.id}> سجل استلام تذكرة في <#${interaction.channel.id}>.`);
   }
 };
